9b57a249f50767eeb56fa73d49661f816e9abf52
Marcus Vinicius <maviniciuus@gmail.com>
2019-01-12 12:48:59 -0200

    [ADD] - Adicionei os samples do tributação br. Removi as custom functions, aplicando a filosofia de aproveitar o máximo os pacotes de terceiros

--- sample/TributacaoBrSample.js
+++
@@ -1,7 +0,0 @@
-const { TributacaoBr } = require('../src/index');
-const color = '\x1b[36m%s\x1b[0m';
-
-//CNPJ
-console.log(color, 'CAPITALIZE =', TributacaoBr.is_cnpj('string minuscula virou capitalize'));
-console.log(color, 'UPPERCASE =', TributacaoBr.uppercase('string minuscula virou uppercase'));
-console.log(color, 'LOWERCASE =', TributacaoBr.lowercase('STRING MAIUSCULA VIROU MINUSCULA'));
--- src/tributacao_br/TributacaoBr.js
+++ src/tributacao_br/TributacaoBr.js
@@ -1,31 +1,28 @@
-const TributacaoBr = {
-	mask_federal_number: value => {
-		value = value.replace(/[^\d]+/g, '');
-		if (!value || !value.length) return value;
+const CPF = require('cpf_cnpj').CPF;
+const CNPJ = require('cpf_cnpj').CNPJ;
 
-		switch (value.length) {
-			case 14:
-				return formatAsCNPJ(value);
-			case 11:
-				return formatAsCPF(value);
-			case 14:
-				return formatAsCEI(value);
-			default:
-				return value;
-		}
+const TributacaoBr = {
+	gen_cpf: (formatted = true) => {
+		return CPF.generate(formatted);
+	},
+	gen_cnpj: (formatted = true) => {
+		return CNPJ.generate(formatted);
 	},
 	mask_cnpj: string => {
-		if (!validateFederalNumber(string)) return string;
+		if (!is_cnpj(string)) return string;
 		return string.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2}).*/, '$1.$2.$3/$4-$5');
 	},
+	unmask_cnpj: cnpj => cnpj.replace(/[^\d]+/g, ''),
 	mask_cpf: string => {
-		if (!validateFederalNumber(string)) return string;
+		if (!is_cpf(string)) return string;
 		return string.replace(/^(\d{3})(\d{3})(\d{3})(\d{2}).*/, '$1.$2.$3-$4');
 	},
+	unmask_cpf: cpf => cpf.replace(/[^\d]+/g, ''),
 	mask_cei: string => {
-		if (!validateFederalNumber(string)) return string;
+		if (!is_cei(string)) return string;
 		return string.replace(/^(\d{2})(\d{3})(\d{3})(\d{3})(\d{1}).*/, '$1.$2.$3.$4-$5');
 	},
+	unmask_cei: cei => cei.replace(/[^\d]+/g, ''),
 	mask_PIS: string => {
 		if (!validatePIS(string)) return string;
 		return string.replace(/[^\d]+/g, '').replace(/^(\d{3})(\d{5})(\d{2})(\d{1}).*/, '$1.$2.$3-$4');
@@ -34,96 +31,36 @@ const TributacaoBr = {
 		if (!string) return string;
 		return string.replace(/[^\d]+/g, '').replace(/^(\d{10})(\d{2}).*/, '$1/$2');
 	},
-	is_federal_number: value => {
+	mask_federal_number: value => {
 		value = value.replace(/[^\d]+/g, '');
-		if (!value) return value;
+		if (!value || !value.length) return value;
 
 		switch (value.length) {
 			case 14:
-				return validateCNPJ(value);
-			case 12:
-				return validateCEI(value);
+				return TributacaoBr.mask_cnpj(value);
 			case 11:
-				return validateCPF(value);
+				return TributacaoBr.mask_cpf(value);
+			case 14:
+				return TributacaoBr.mask_cei(value);
 			default:
-				return false;
+				return value;
 		}
+	},
+	is_federal_number: value => {
+		value = value.replace(/[^\d]+/g, '');
+		if (!value) return false;
+
+		if (value.length === 14) return TributacaoBr.is_cnpj(value);
+		if (value.length === 12) return TributacaoBr.is_cei(value);
+		if (value.length === 11) return TributacaoBr.is_cpf(value);
+
+		return false;
 	},
 	is_cpf: cpf => {
-		cpf = cpf.replace(/[^\d]+/g, '');
-		if (!cpf || !cpf.length) return false;
-
-		if (
-			cpf === '00000000000' ||
-			cpf === '11111111111' ||
-			cpf === '22222222222' ||
-			cpf === '33333333333' ||
-			cpf === '44444444444' ||
-			cpf === '55555555555' ||
-			cpf === '66666666666' ||
-			cpf === '77777777777' ||
-			cpf === '88888888888' ||
-			cpf === '99999999999'
-		)
-			return false;
-
-		let soma = 0;
-		let resto;
-
-		for (i = 1; i <= 9; i++) soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
-		resto = (soma * 10) % 11;
-
-		if (resto === 10 || resto === 11) resto = 0;
-		if (resto !== parseInt(cpf.substring(9, 10))) return false;
-
-		soma = 0;
-		for (i = 1; i <= 10; i++) soma = soma + parseInt(value.substring(i - 1, i)) * (12 - i);
-		resto = (soma * 10) % 11;
-
-		if (resto === 10 || resto === 11) resto = 0;
-		return resto === parseInt(cpf.substring(10, 11));
+		return CPF.isValid(cpf);
 	},
 	is_cnpj: cnpj => {
-		cnpj = cnpj.replace(/[^\d]+/g, '');
-		if (!cnpj || cnpj.length !== 14) return false;
-
-		if (
-			cnpj === '00000000000000' ||
-			cnpj === '11111111111111' ||
-			cnpj === '22222222222222' ||
-			cnpj === '33333333333333' ||
-			cnpj === '44444444444444' ||
-			cnpj === '55555555555555' ||
-			cnpj === '66666666666666' ||
-			cnpj === '77777777777777' ||
-			cnpj === '88888888888888' ||
-			cnpj === '99999999999999'
-		)
-			return false;
-
-		// Valida DVs
-		tamanho = cnpj.length - 2;
-		numeros = cnpj.substring(0, tamanho);
-		digitos = cnpj.substring(tamanho);
-		soma = 0;
-		pos = tamanho - 7;
-		for (i = tamanho; i >= 1; i--) {
-			soma += numeros.charAt(tamanho - i) * pos--;
-			if (pos < 2) pos = 9;
-		}
-		resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-		if (resultado !== digitos.charAt(0)) return false;
-
-		tamanho = tamanho + 1;
-		numeros = cnpj.substring(0, tamanho);
-		soma = 0;
-		pos = tamanho - 7;
-		for (i = tamanho; i >= 1; i--) {
-			soma += numeros.charAt(tamanho - i) * pos--;
-			if (pos < 2) pos = 9;
-		}
-		resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-		return resultado === digitos.charAt(1);
+		return CNPJ.isValid(cnpj);
 	},
 	is_cei: cei => {
 		cei = cei.replace(/[^\d]+/g, '');
