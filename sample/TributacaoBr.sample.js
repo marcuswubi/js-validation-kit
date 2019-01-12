d8b46c7e1a503aa3b2d4245890bbd64928c8888b
Marcus Vinicius <maviniciuus@gmail.com>
2019-01-12 12:49:26 -0200

    [INC] - Inclusão de arquivos

---
+++ sample/TributacaoBr.sample.js
@@ -0,0 +1,14 @@
+const { TributacaoBr } = require('../src/index');
+const color = '\x1b[36m%s\x1b[0m';
+
+//CPF
+const cpf = TributacaoBr.gen_cpf();
+console.log(color, 'CPF GENERATOR =', cpf);
+console.log(color, 'IS CPF =', cpf, TributacaoBr.is_cpf(cpf));
+console.log(color, 'IS FEDERAL NUMBER =', cpf, TributacaoBr.is_federal_number(cpf));
+
+//CNPJ
+const cnpj = TributacaoBr.gen_cnpj();
+console.log(color, 'CNPJ GENERATOR =', cnpj);
+console.log(color, 'IS CNPJ =', cnpj, TributacaoBr.is_cnpj(cnpj));
+console.log(color, 'IS FEDERAL NUMBER =', cnpj, TributacaoBr.is_federal_number(cnpj));
