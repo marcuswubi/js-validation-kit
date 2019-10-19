5cb401b3aef77f4a79e1ef756a3a6262291f68d7
evandro_gdn <evandrogardolinn@gmail.com>
2019-10-19 08:56:41 -0300

    [UPD] - Modificado a chamada dos métodos para a classe PessoaFisica, para manter a compatibilidade em caso de usos da mesma

--- src/documentos/PessoaFisica.js
+++ src/documentos/PessoaFisica.js
@@ -1,13 +1,10 @@
-const { CPF } = require('cpf_cnpj');
+const TributacaoBr = require('../tributacao_br/TributacaoBr.js');
 
 const PessoaFisica = {
-  gencpf: (formated = true) => CPF.generate(formated),
-  isCpf: (cpf) => CPF.isValid(cpf),
-  unmaskCpf: (cpf) => cpf.replace(/[^\d]+/g, ''),
-  maskCpf: (string) => {
-    if (!PessoaFisica.isCpf(string)) return string;
-    return string.replace(/^(\d{3})(\d{3})(\d{3})(\d{2}).*/, '$1.$2.$3-$4');
-  },
+  gencpf: (formated = true) => TributacaoBr.genCpf(formated),
+  isCpf: (cpf) => TributacaoBr.isCpf(cpf),
+  unmaskCpf: (cpf) => TributacaoBr.unmaskCpj(cpf),
+  maskCpf: (unmaskedCpf) => TributacaoBr.maskCpj(unmaskedCpf),
 };
 
 module.exports = PessoaFisica;
